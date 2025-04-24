98c4ef18d6094753a54f45262c117bcf55babbc2
user <user@MacBook-Pro-user.local>
2025-04-24 21:53:59 +0300

    Added product description

---
+++ lib/pages/details/product_details_description.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailsDescription extends StatelessWidget {
+  const ProductDetailsDescription({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(),
+        Container(),
+        Container(),
+        Container(),
+        Container()
+      ],
+    );
+  }
+
+}
